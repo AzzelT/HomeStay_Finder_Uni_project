00106e25dac96f38627d0de8a5bcf4b253fc5359
AzzelT <toletech@pm.me>
2026-09-14 15:56:24 +0700

    add hotel.php to model

---
+++ app/Models/Hotel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    protected $fillable = [
+        'province_id',
+        'user_id',
+        'name',
+        'description',
+        'price_per_night',
+        'address',
+        'star_rating',
+        'website_url',
+        'facebook_url',
+        'google_maps_url',
+    ];
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(HotelImage::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function amenities()
+    {
+        return $this->belongsToMany(Amenity::class, 'hotel_amenities');
+    }
+}
