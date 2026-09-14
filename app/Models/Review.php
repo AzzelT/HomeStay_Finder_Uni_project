864c12c75d70d7dcf2bf945ad247558b4346310f
AzzelT <toletech@pm.me>
2026-09-14 15:03:22 +0700

    updated the model folder

---
+++ app/Models/Review.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    protected $fillable = ['hotel_id', 'user_id', 'rating', 'comment'];
+
+    public function hotel()
+    {
+        return $this->belongsTo(Hotel::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
